9a1e80e07b9f2927bf316b7e335d7cbfff7f6e4f
mohammadjalizi <mjalizi1376@gmail.com>
2023-01-17 12:05:12 +0330

    addproject

--- src/Copmenen/Proudouct.jsx
+++ src/Copmenen/Proudouct.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 function Proudouct(props) {
     if(props.price>100){
+
+        let{imges,name,price}=props
         return (
             <div className='  '>
         <div className=' rounded-3xl bg-slate-500 h-auto  m-2 '>
-        <img src={props.imges}/>
-        <h1>{props.name}</h1>
-        <p>${props.price}</p>
+        <img src={imges}/>
+        <h1>{name}</h1>
+        <p>${price}</p>
         </div>
         
         
