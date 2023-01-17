9991e7daad2916735cc92ae58dee83717cee30f4
mohammadjalizi <mjalizi1376@gmail.com>
2023-01-17 11:45:08 +0330

    addproject

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import img1 from './Copmenen/images/Album 1.png'
 import './App.css';
+import Proudouct from './Copmenen/Proudouct';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  className=' flex flex-wrap  justify-center '>
+    
+    <Proudouct name="ali"  title="aliajaliada;ja;;da"  price={120} imges={img1} />
+    <Proudouct name="ali"  title="aliajaliada;ja;;da"  price={120} imges={img1} />
+    <Proudouct name="ali"  title="aliajaliada;ja;;da"  price={120} imges={img1} />
+    <Proudouct name="ali"  title="aliajaliada;ja;;da"  price={120} imges={img1} />
+    <Proudouct name="ali"  title="aliajaliada;ja;;da"  price={120} imges={img1} />
+    <Proudouct name="ali"  title="aliajaliada;ja;;da"  price={120} imges={img1} />
     </div>
+ 
   );
 }
 
--- src/Copmenen/Proudouct.jsx
+++ src/Copmenen/Proudouct.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
-function Proudouct() {
+function Proudouct(props) {
   return (
-    <div>Proudouct</div>
+    <div className='  '>
+<div className=' rounded-3xl bg-slate-500 h-auto  m-2 '>
+<img src={props.imges}/>
+<h1>{props.name}</h1>
+<p>${props.price}</p>
+</div>
+
+
+    </div>
   )
 }
 
