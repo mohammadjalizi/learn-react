c19ba67c952d0af79ed5ba1c99f1ae84a5bebd00
mohammadjalizi <mjalizi1376@gmail.com>
2023-01-26 09:47:28 +0330

    added project

--- src/App.js
+++ src/App.js
@@ -4,32 +4,50 @@ import img3 from './Copmenen/images/Album 3.png'
 import './App.css';
 import Proudouct from './Copmenen/Proudouct';
 import Modal from './Copmenen/Modal';
+import React from 'react';
 
-function App() {
+    class  App extends  React.Component {
 
-
-  const allproudcts=[
-
-{id:1,name:'mohammad', price:30 , imges:img1},
-{id:2,name:'ali', price:105 , imges:img2},
-{id:3,name:'acer', price:90 , imges:img3},
-
-  ]
-  return (
-    <div  className=' flex flex-wrap  justify-center '>
-    
-    <Proudouct {...allproudcts[0]} >
-      <button>  off(50%)   </button>
-       </Proudouct>
-    <Proudouct {...allproudcts[1]} > </Proudouct>
-    <Proudouct {...allproudcts[2]} > </Proudouct>
-
-    <Modal> 
-              
-    <h1 className='  text-center'>  hello   </h1> </Modal>
-    </div>
  
-  );
-}
+ constructor (props){
 
-export default App;
+super(props)
+this.state={
+
+  users:[
+    {id:1,name:'mohammad', price:30 , imges:img1},
+    {id:2,name:'ali', price:105 , imges:img2},
+    {id:3,name:'acer', price:90 , imges:img3},
+  ]
+}
+this.code=20
+ }
+
+     
+
+    render() {  
+      return (
+        <div  className=' flex flex-wr`ap  justify-center '>
+        {this.code}
+        <Proudouct {...this.state.users[0]} >
+          <button>  off(50%)   </button>
+          </Proudouct>
+          <Proudouct {...this.state.users[0]} > </Proudouct>
+          <Proudouct {...this.state.users[0]} > </Proudouct>
+  
+        <Modal> 
+                  
+        <h1 className='  text-center'>  hello   </h1> </Modal>
+        </div>
+    
+      );
+    }
+  
+  
+
+  }
+export default App
+
+// 
+
+
