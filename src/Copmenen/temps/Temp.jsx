8e65c65d678c6fb93c3ac4d0bd22aae8da9449e4
mohammadjalizi <mjalizi1376@gmail.com>
2023-01-28 19:21:14 +0330

    added project

--- src/Copmenen/temps/Temp.jsx
+++ src/Copmenen/temps/Temp.jsx
@@ -2,69 +2,20 @@ import { Component } from "react";
 
 export default  class Temp  extends Component{
 
-  constructor (props){
-super(props)
-
-this.state={
-
-    Temp:10,
-    tempclass:'cold'
-
-
-}
-this.Addplus=this.Addplus.bind(this)
-this.Minus=this.Minus.bind(this)
-
-  }
-
-
-Addplus(){
-if(this.state.Temp>30){
-return false
-
-}
-this.setState(prevestate=>{
-return {Temp:prevestate.Temp + 1}
-
-})
-if(this.state.Temp >=15){
-
-    this.setState({tempclass:'hot'})
-}
-}
-Minus(){
-    if(this.state.Temp<0){
-        return false
-        
-        }
-        this.setState(prevestate=>{
-        return {Temp:prevestate.Temp- 1}
-        
-        })
-        if(this.state.Temp <=15){
-
-            this.setState({tempclass:'cold'})
-        }
-}
 
 render(){
 
 return(
     <div className=" bg-green-800  w-[50%] m-auto h-3/5 ">
 
-<div>
+<div className="">
+    10 c
 
-    <div className={`  p-5  ${this.state.tempclass}`}>
-
-{this.state.Temp}
-    </div>
-    <div className=" flex justify-between ">
-
-        <button  onClick={this.Addplus}>+</button>
-        <button onClick={this.Minus}>-   </button>
-    </div>
 </div>
-
+<div className=" flex justify-between">
+<button> +</button>
+<button>-</button>
+</div>
 
     </div>
 
