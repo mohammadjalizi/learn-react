09a8d822c9275eb9dfcd021a7ead2984a95275ae
mohammadjalizi <mjalizi1376@gmail.com>
2023-01-31 10:56:47 +0330

    added project

--- src/Copmenen/Frome/Frome.jsx
+++ src/Copmenen/Frome/Frome.jsx
@@ -9,12 +9,14 @@ super(props)
 
 
 this.state={
-username:''
+username:'',
+texterae:''
 
 }
 
 
 this.CLICKCHng=this.CLICKCHng.bind(this)
+this.chanchtext=this.chanchtext.bind(this)
     }
     CLICKCHng(event){
 console.log(event)
@@ -29,14 +31,20 @@ username:event.nativeEvent.target.value
 
     
     }
+    chanchtext(event){
+
+        console.log(event)
+        
+        }
   render() {
     return (
       <div >
 <div>
+    <h1>  {this.state.username}  </h1>
     <input type="text"  value={this.state.username} onChange={this.CLICKCHng}  className=" bg-slate-500 "/>
     </div>
 
-
+<textarea value={this.state.texterae}   onChange={this.chanchtext}> </textarea>
       </div>
     )
   }
