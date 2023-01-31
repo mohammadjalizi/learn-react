295d3c9d34807f35b70d3559567e3216ba5d3c5d
mohammadjalizi <mjalizi1376@gmail.com>
2023-01-31 10:15:46 +0330

    added project

---
+++ src/Copmenen/Frome/Frome.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default class Frome extends Component {
+
+    constructor(prosp){
+super(props)
+this.state={
+username:''
+
+}
+
+        
+    }
+  render() {
+    return (
+      <div>
+<div><input type="text" name="" id="" /></div>
+
+
+      </div>
+    )
+  }
+}
