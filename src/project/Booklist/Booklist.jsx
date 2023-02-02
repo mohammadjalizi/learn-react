be5cff7c662957be25e2793554bf91e5ac470505
mohammadjalizi <mjalizi1376@gmail.com>
2023-02-02 15:22:22 +0330

    added project

--- src/project/Booklist/Booklist.jsx
+++ src/project/Booklist/Booklist.jsx
@@ -5,9 +5,20 @@ export class Booklist extends Component {
     return (
       <div>
 
-<h1 className='  flex justify-center  w-full '>  <BsBookFill   className=' mt-1 mr-6   text-yellow-300    '/>Booklist    </h1>
+<h1 className='  flex justify-center  border-b-2 border-blue-400 my-16  w-full '>  <BsBookFill   className=' mt-1 mr-6   text-yellow-300    '/>Booklist    </h1>
+<div className=' w-[70%] m-auto bg-orange-500 p-16 '>   
+<div className=' my-14 '>
+<h1>tittle</h1>
+<input type=""  className=' rounded-3xl  border  w-full '/>
 
+</div>
+<div>
+<h1>tittle</h1>
+<input type=""  className='  rounded-3xl w-full '/>
 
+</div>  
+<button>Add Book</button>
+</div>
       </div>
     )
   }
