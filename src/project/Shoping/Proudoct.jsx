3a8d775cad4d029778058bd541a7bd6763b08339
mohammadjalizi <mjalizi1376@gmail.com>
2023-02-07 13:12:14 +0330

    Added project

---
+++ src/project/Shoping/Proudoct.jsx
@@ -0,0 +1,29 @@
+import { Component } from "react";
+
+
+export  default class Product extends Component{
+
+
+render(){
+
+
+    return(
+
+
+
+        <div>
+            <spa> shirt</spa>
+            <img src="../"></img>
+            <div>
+                <span>%129</span>
+                <button type="button">
+
+                    Add to cart
+                </button>
+            </div>
+        </div>
+    )
+}
+
+
+}
